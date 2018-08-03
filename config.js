56c99174f74283c5cec7420151f98abfab09c85f
Joey Kathan <kathan.joey@gmail.com>
2018-08-02 21:10:57 -0400

    initial commit

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app-mongoose';
-exports.PORT = process.env.PORT || 8080;
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://jkathan:Milk68not!@ds263791.mlab.com:63791/blog-app';
+exports.PORT = process.env.PORT || 63791;
