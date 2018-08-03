b1943e27f25caa7e0eb157d3213d2a423e9399aa
Joey Kathan <kathan.joey@gmail.com>
2018-08-02 21:53:57 -0400

    fixing bugs

--- server.js
+++ server.js
@@ -143,8 +143,8 @@ function closeServer() {
   });
 }
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+app.listen(process.env.PORT || 13191, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 13191`);
 });
 
 if (require.main === module) {
